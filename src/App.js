7f4c30ebed5579df65540d42a8a29b8c8d703efe
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-05-01 13:02:03 +0200

    todolist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TodoItems = ({ entries }) => (
+  <div className="container">
+    <ul className="list-group ">
+    {entries.map(({ heading, key }) => (
+      <li className="list-group-item" key={key}>{heading}</li>
+    ))}
+   </ul>
+  </div>
+  
+);
+
+
+class App extends React.Component{
+  state = {
+    items : []
+  }
+  addTodo = (heading) => heading !== '' && this.setState(({ items }) => ({
+    items: items.concat({ 
+      heading,
+      key: Date.now()
+    })
+  }));
+   
+  render(){
+
+    return (
+      <div className="container-fluid bg-light">
+        <TodoItems entries={this.state.items} />
+       <InputForm onSubmit={this.addTodo}></InputForm>
+     </div>
+        ) 
+  }
+}
+
+
+
+class InputForm extends React.Component{
+  state = {
+    input: ''
+  };
+
+  onInput = (e) => this.setState({
+    input: e.target.value
+  });
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.input);
+  }
+  render(){
+    
+
+    return (
+        <div className="container">
+           <hr/>
+          <form className="form-inline" onSubmit={this.onSubmit}>
+          <label>Task</label>
+           <div className="form-group mx-sm-2">
+            <input type="text" className="form-control"  placeholder="Task name" 
+
+              value={ this.state.value }
+              onChange={ this.onInput } />
+            </div>
+            <button  type="submit" className="btn btn-primary "
+            >Add To List</button>
+            
+          </form>
+        </div>
+        
+      
+      
+    )
+  }
 }
 
 export default App;
